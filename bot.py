ae538372e3c92355368949e0722b3776e6692d01
MrKomodoDragon <svrchn921@outlook.com>
2022-04-05 14:14:34 -0700

    New STuff IDK mate

--- bot.py
+++ bot.py
@@ -6,5 +6,10 @@ import jishaku
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = ComicsBot(command_prefix="comix ", help_command=commands.MinimalHelpCommand(), intents=intents, application_id=826125255734722580)
-bot.run(toml.load("config.toml")['token'])
+bot = ComicsBot(
+    command_prefix="comix ",
+    help_command=commands.MinimalHelpCommand(),
+    intents=intents,
+    application_id=826125255734722580,
+)
+bot.run(toml.load("config.toml")["token"])
--- core/custombot.py
+++ core/custombot.py
@@ -12,28 +12,29 @@ class ComicsBot(commands.Bot):
         super().__init__(*args, **kwargs)
         self.afk = {}
         self.loop = asyncio.get_event_loop()
-        self.config = toml.load('config.toml')
+        self.config = toml.load("config.toml")
         self.started = False
 
     async def get_context(self, message, *, cls=None):
         return await super().get_context(message, cls=cls or ComicsContext)
 
     async def start(self, token, *, reconnect=True):
-        await self.load_extension('jishaku')
+        await self.load_extension("jishaku")
         await self.load_extension("cogs.gocomics")
         import os
 
-        os.environ['JISHAKU_NO_UNDERSCORE'] = 'True'
-        os.environ['JISHAKU_NO_DM_TRACEBACK'] = 'True'
-        os.environ['JISHAKU_HIDE'] = 'True'
-        await self.reload_extension('jishaku')
+        os.environ["JISHAKU_NO_UNDERSCORE"] = "True"
+        os.environ["JISHAKU_NO_DM_TRACEBACK"] = "True"
+        os.environ["JISHAKU_HIDE"] = "True"
+        await self.reload_extension("jishaku")
         return await super().start(token, reconnect=reconnect)
 
     async def setup_hook(self) -> None:
         self.session = aiohttp.ClientSession()
         return await super().setup_hook()
+
     async def close(self):
-        await self.session.close()
+        # await self.session.close() if self.session else print("no sessino")
         return await super().close()
 
     async def on_message_edit(self, before, after):
@@ -41,6 +42,7 @@ class ComicsBot(commands.Bot):
 
     async def on_ready(self):
         print("The bot is ready")
-       # if not self.started:
-        #    await self.tree.sync(guild=discord.Object(id=842459680436781078))
-        #    self.started = True
+
+    # if not self.started:
+    #    await self.tree.sync(guild=discord.Object(id=842459680436781078))
+    #    self.started = True
