569ac2786f57f9166c2014c8de41382f64bece5e
Tom-the-Bomb <77308095+Tom-the-Bomb@users.noreply.github.com>
2022-04-30 09:47:22 -0400

    cleaner coedee!

--- core/custombot.py
+++ core/custombot.py
@@ -1,31 +1,33 @@
 import asyncio
+import os
 
-import aiohttp
-import toml
 from discord.ext import commands
 import discord
+import toml
+
 from .context import ComicsContext
 
 
 class ComicsBot(commands.Bot):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
+        
         self.afk = {}
         self.loop = asyncio.get_event_loop()
         self.config = toml.load("config.toml")
-        self.started = False
+        self.started: bool = False
 
-    async def get_context(self, message, *, cls=None):
+    async def get_context(self, message, *, cls: type = None):
         return await super().get_context(message, cls=cls or ComicsContext)
 
-    async def start(self, token, *, reconnect=True):
+    async def start(self, token, *, reconnect: bool = True):
         await self.load_extension("jishaku")
         await self.load_extension("cogs.gocomics")
-        import os
 
         os.environ["JISHAKU_NO_UNDERSCORE"] = "True"
         os.environ["JISHAKU_NO_DM_TRACEBACK"] = "True"
         os.environ["JISHAKU_HIDE"] = "True"
+        
         await self.reload_extension("jishaku")
         return await super().start(token, reconnect=reconnect)
 
@@ -37,10 +39,7 @@ class ComicsBot(commands.Bot):
         # await self.session.close() if self.session else print("no sessino")
         return await super().close()
 
-    async def on_message_edit(self, before, after):
-        ...
-
-    async def on_ready(self):
+    async def on_ready(self) -> None
         print("The bot is ready")
 
     # if not self.started:
